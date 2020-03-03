8e0b07c81764cc1ffa1f37ff874a1f67a94d9bf4
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-03-02 19:10:31 -0500

    Added in basic header elements

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <img class="logo" src="./assets/img/logo.png" alt="Website Logo" />
+      <nav>
+        <ul class="header-nav">
+          <li
+            v-for="tab in tabs"
+            :key="tab.title"
+          >
+            <router-link
+              tag="a"
+              v-bind:to="tab.url"
+              v-bind:exact="tab.title === 'Home' ? true : false"
+            >
+              {{ tab.title }}
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  data() {
+    return {
+      tabs: [
+        {
+          title: "Home",
+          url: "/",
+          left: true
+        },
+        {
+          title: "Bio",
+          url: "/bio",
+          left: true
+        },
+        {
+          title: "Portfolio",
+          url: "/portfolio",
+          left: true
+        },
+        {
+          title: "Contact Me",
+          url: "/contact-me",
+          left: false
+        }
+      ]
+    };
   }
-}
-</style>
+};
+</script>
