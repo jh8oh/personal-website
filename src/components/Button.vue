38cb2eaffe5af17a34b6ba2f9c268c0baf11e6fa
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-05-11 05:28:43 -0400

    Drafted home page

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button" :class="'class'">
+    <h4>{{ text }}</h4>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    class: {
+      type: String,
+      default: "",
+    },
+    text: {
+      type: String,
+    },
+  },
+};
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,20 @@
 <template>
-  <div></div>
+  <div id="home">
+    <div id="greeting">
+      <h4>Hi, I'm</h4><br />
+      <h2>Ji Ho Oh</h2><br />
+      <h4>Programmer, Artist, World-building enthusiast</h4>
+      <Button :text="'Get to know me'"/>
+    </div>
+  </div>
 </template>
+
+<script>
+import Button from "../components/Button.vue"
+
+export default {
+  components: {
+    Button
+  }
+}
+</script>
