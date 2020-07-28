d67173834ddc8e2b72891b948006e4670c683713
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-07-28 16:14:10 -0400

    Scroll-to-top only appears when user scrolls 20px.

--- src/layouts/ScrollToTop.vue
+++ src/layouts/ScrollToTop.vue
@@ -1,5 +1,29 @@
 <template>
-	<a id="scroll-to-top" href="#top">
-		<img src="../assets/ico/scroll-to-top/arrow-up-dark.png"/>
-	</a>
+  <a id="scroll-to-top" ref="scrollToTop" href="#top">
+    <img src="../assets/ico/scroll-to-top/arrow-up-dark.png" />
+    <span>Back To Top</span>
+  </a>
 </template>
+
+<script>
+export default {
+  created() {
+    window.addEventListener("scroll", this.toggleScrollToTop);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.toggleScrollToTop);
+  },
+  methods: {
+    toggleScrollToTop() {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        this.$refs.scrollToTop.style.display = "flex";
+      } else {
+        this.$refs.scrollToTop.style.display = "none";
+      }
+    },
+  },
+};
+</script>
