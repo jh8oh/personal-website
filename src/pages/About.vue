36cec3c18c22292d1cb699835262be5a04d11085
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-08-04 13:10:20 -0400

    Refactored heading and containers

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,25 +1,30 @@
 <template>
   <div id="about">
     <section id="intro" class="page">
-      <h1>About Me</h1>
-      <div id="intro-content">
-        <img src="../assets/img/about/profile-pic.jpg" alt="Profile Picture" />
-        <p>
-          Hello! I'm Ji Ho Oh (오지호), or Jay if that's easier to pronounce,
-          and I'm a 2nd-year student at the University of Waterloo. I like
-          making user-facing software, though the user's me most of the time.
-          This has led to me building websites, Android applications, and games.
-          I also enjoy learning new technologies and expanding my repertoire of
-          libraries, frameworks, and languages. Aside from coding, my hobbies
-          include playing video games, reading manga, and world-building. One of
-          my goals is to learn how to compose music, specifically BGM for video
-          games.
-        </p>
+      <div class="container">
+        <h1>About Me</h1>
+        <div id="intro-content">
+          <img
+            src="../assets/img/about/profile-pic.jpg"
+            alt="Profile Picture"
+          />
+          <p>
+            Hello! I'm Ji Ho Oh (오지호), or Jay if that's easier to pronounce,
+            and I'm a 2nd-year student at the University of Waterloo. I like
+            making user-facing software, though the user's me most of the time.
+            This has led to me building websites, Android applications, and
+            games. I also enjoy learning new technologies and expanding my
+            repertoire of libraries, frameworks, and languages. Aside from
+            coding, my hobbies include playing video games, reading manga, and
+            world-building. One of my goals is to learn how to compose music,
+            specifically BGM for video games.
+          </p>
+        </div>
       </div>
       <NextSectionButton :href="'#experience'" />
     </section>
     <section id="experience" class="background-alt">
-      <div id="experience-container">
+      <div class="container">
         <h2>Experiences</h2>
         <div id="experience-content">
           <ul id="timeline">
@@ -42,7 +47,11 @@
       </div>
       <NextSectionButton :href="'#interests'" />
     </section>
-    <section id="interests"></section>
+    <section id="interests">
+      <div class="container">
+        <h2>Interests</h2>
+      </div>
+    </section>
     <Footer />
   </div>
 </template>
