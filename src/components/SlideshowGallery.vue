77619ac388dbf9b83f44a8465f96a6e2f82f5e3d
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-08-16 20:24:03 -0400

    HTML for slideshow gallery

---
+++ src/components/SlideshowGallery.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="slideshow-gallery">
+    <img :src="leftIcon" @click="back()" />
+    <slot />
+    <img :src="rightIcon" @click="next()" />
+  </div>
+</template>
+
+<script>
+import arrowLeftDark from "../assets/ico/components/slideshow-gallery/arrow-left-dark.png";
+import arrowLeftAccent from "../assets/ico/components/slideshow-gallery/arrow-left-accent.png";
+import arrowRightDark from "../assets/ico/components/slideshow-gallery/arrow-right-dark.png";
+import arrowRightAccent from "../assets/ico/components/slideshow-gallery/arrow-right-accent.png";
+
+export default {
+  data() {
+    return {
+      leftIcon: arrowLeftDark,
+      rightIcon: arrowRightDark,
+    };
+  },
+  methods: {
+    back() {
+      this.leftIcon = arrowLeftAccent;
+      setTimeout(() => this.$emit("back"), 100);
+      this.leftIcon = arrowLeftDark;
+    },
+    next() {
+      this.rightIcon = arrowRightAccent;
+      setTimeout(() => this.$emit("next"), 100);
+      this.rightIcon = arrowRightDark;
+    },
+  },
+};
+</script>
