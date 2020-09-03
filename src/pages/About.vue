b41eaf008fcd7023a6451e2ee3518de415e08df1
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-09-02 21:24:25 -0400

    Small changes. Better shutter for intro.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,17 +5,19 @@
         <h1>About Me</h1>
         <div id="intro-content">
           <img src="../assets/img/about/profile-pic.jpg" alt="Profile Picture" />
-          <p>
-            Hello! I'm Ji Ho Oh (오지호), or Jay if that's easier to pronounce, and I'm a 2nd-year student at the
-            University of Waterloo. I like making user-facing software, though the user's me most of the time. This has
-            led to me building websites, Android applications, and games. I also enjoy learning new technologies and
-            expanding my repertoire of libraries, frameworks, and languages. Aside from coding, my hobbies include
-            playing video games, reading manga, and world-building. One of my goals is to learn how to compose music,
-            specifically BGM for video games.
-          </p>
+          <div id="summary-container">
+            <p>
+              Hello! I'm Ji Ho Oh (오지호), or Jay if that's easier to pronounce, and I'm a 2nd-year student at the
+              University of Waterloo. I like making user-facing software, though the user's me most of the time. This
+              has led to me building websites, Android applications, and games. I also enjoy learning new technologies
+              and expanding my repertoire of libraries, frameworks, and languages. Aside from coding, my hobbies include
+              playing video games, reading manga, and world-building. One of my goals is to learn how to compose music,
+              specifically BGM for video games.
+            </p>
+          </div>
         </div>
       </div>
-      <NextSectionButton :href="'#experience'" />
+      <NextSectionButton :href="'#abilities'" />
     </section>
     <section id="abilities" class="background-alt">
       <div class="container">
