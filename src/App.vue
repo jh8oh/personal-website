a3f508a46401e85426e232639d44114726e9f086
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-10-27 03:00:59 -0400

    Created header html

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,6 @@
   </div>
 </template>
 
-<<<<<<< HEAD
-<style lang="scss" src="./scss/main.scss" />
-=======
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Header from "@/layouts/Header.vue";
@@ -18,5 +15,4 @@ import Header from "@/layouts/Header.vue";
 export default class App extends Vue {}
 </script>
 
-<style lang="scss" src="./scss/main.scss"/>
->>>>>>> c713c83 (init header)
+<style lang="scss" src="./scss/main.scss" />
--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,6 +1,40 @@
 <template>
-	<header>
-
-	</header>
+  <header>
+    <router-link to="/">
+      <img @mouseover="setLogo(true)" @mouseleave="setLogo(false)" :src="logoIcon" alt="Logo" />
+    </router-link>
+    <nav>
+      <ul>
+        <li v-for="page in pages" :key="page.title">
+          <router-link :to="page.url" exact="false" />
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import logo from "@/assets/ico/header/logo-border-white.png";
+import logoHovered from "@/assets/ico/header/logo-border-accent.png";
+
+@Component
+export default class Header extends Vue {
+  private logoIcon = logo;
+  private pages = [
+    { title: "About Me", url: "/about" },
+    { title: "Portfolio", url: "/portfolio" },
+    { title: "Blog", url: "/blog" },
+    { title: "Contact Me", url: "/contact" },
+  ];
+
+  private setLogo(isLogoHovered: boolean) {
+    if (isLogoHovered) {
+      this.logoIcon = logoHovered;
+    } else {
+      this.logoIcon = logo;
+    }
+  }
+}
+</script>
