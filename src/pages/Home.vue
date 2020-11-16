9ad1f1bdf34245cef1e9c2b3024960d760da6014
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-11-15 22:16:03 -0500

    Added button to home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="home">
-    <img id="background-image" src="@/assets/img/home/logo-border-base.png"/>
+    <img id="background-image" src="@/assets/img/home/logo-border-base.png" />
     <div id="greeting">
       <span>Hi, I'm</span><br />
-      <span id="name">{{ name }}</span><br />
+      <span id="name">{{ name }}</span
+      ><br />
       <span>Software developer, Programmer, Designer</span>
       <br /><br />
+      <Button :text="'Get to know me'" @clicked="goToAbout()" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import Button from "@/components/Button.vue";
 
-@Component
+@Component({
+  components: { Button },
+})
 export default class Home extends Vue {
-  private name = "Ji Ho Oh"
+  private name = "Ji Ho Oh";
+
+  private goToAbout() {
+    this.$router.push("/about");
+  }
 }
 </script>
