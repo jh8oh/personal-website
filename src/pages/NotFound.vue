a63a6b696896e0bc773f760fec4f1d63d7491a96
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-11-19 12:23:23 -0500

    NotFound content

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div id="content">
+      <span>404</span>
+      <Button :text="'Back to home'" @clicked="goToHome()" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Button from "@/components/Button.vue";
+
+@Component({
+  components: { Button },
+})
+export default class NotFound extends Vue {
+  private goToHome(){
+    this.$router.push("/");
+  }
+}
+</script>
