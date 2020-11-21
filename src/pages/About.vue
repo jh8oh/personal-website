f4bb43aa0c212c95c5d46d91890e13dedb0dc55f
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-11-20 21:04:58 -0500

    Set up About Me page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about">
+    <h1>About Me</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class About extends Vue {}
+</script>
