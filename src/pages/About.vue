2ac2cea181334fa27f160e2899af9130b6ddf1af
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-11-24 15:19:13 -0500

    Changed profile-pic img. CSS for about intro.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="about" class="page">
-    <section id="summary">
-      <h1>About Me</h1>
+    <h1>About Me</h1>
+    <section id="intro">
       <p>
         Hello! I'm Ji Ho Oh (오지호) but I also go by Jay. I'm a 3rd-year undergrad at the University of Waterloo.<br /><br />
-        I like creating user-facing software, UI/UX stuff, mostly because I want the things I make to look
-        <em>æsthetic</em>. This has led to me building websites, Android applications, and games. I also like learning
+        I like creating user-facing software, UI/UX and all that, mostly because I want the things I make to look
+        <em>a e s t h e t i c</em>. This has led to me building websites, Android applications, and games. I also like learning
         new libraries and frameworks, preferring to learn a little about a lot of things over specializing in one
-        aspect. Jack of all trades and all that.<br /><br />Aside from coding, my hobbies include playing video games,
+        aspect.<br /><br />Aside from coding, my hobbies include playing video games,
         reading manga, and world-building.
       </p>
-      <img src="../assets/img/about/profile-pic.jpg" alt="Profile Picture" />
+      <img src="../assets/img/about/profile-pic.png" alt="Profile Picture" />
     </section>
   </div>
 </template>
