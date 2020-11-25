3780e4859b361b56bdb57f5c602ba057f8aa38f5
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-11-24 22:26:35 -0500

    Experience HTML

---
+++ src/assets/content/about/experience.ts
@@ -0,0 +1,39 @@
+class Experience {
+  id: number;
+  employer: string;
+  position: string;
+  website: string;
+  dates: string;
+  description: string;
+
+  constructor(id: number, employer: string, position: string, website: string, dates: string, description: string) {
+    this.id = id;
+    this.employer = employer;
+    this.position = position;
+    this.website = website;
+    this.dates = dates;
+    this.description = description;
+  }
+}
+
+const dcCanada = new Experience(
+  0,
+  "DC Canada Education Publishing",
+  "Web and Mobile Developer",
+  "https://www.dc-canada.ca/",
+  "January - April 2020",
+  "This was my first internship/co-op position.<br /><strong>Web developer</strong><ul><li>Maintained and developed new contents and features on a WordPress powered website</li><li>Integrated MotionPay into the payment system for the Chinese domain which allowed customers to pay by scanning a QR code in Alipay/WeChat pay</ul><strong>Mobile application developer</strong><ul><li>Solo development and design of an Android eBook reader titled 'One Story a Day'</li><li>Connects with the LMS on the <a href=\"https://onestoryaday.ca\">website</a></li></ul>"
+);
+
+const innovapost = new Experience(
+  1,
+  "Innovapost",
+  "Android/Innovation Developer",
+  "https://innovapost.com/",
+  "September 2020 - current",
+  "Split between working on the Android-platform team and the 'Innovation Garage'.<br /><strong>Android</strong><ul><li>Fixed production bugs and implemented new features for the Canada Post application</li><li>Worked in a agile-scrum environment</li></ul><strong>Innovation Garage</strong><ul><li>Researched the possibility of implementing progressive registration and OAuth 2.0 on the app</li><li>Part of a pilot team to eliminate the need for ring scanners and instead use wrist-mounted PDTs which automatically (de)activate their scanner based on the accelerometer.</li></ul>"
+);
+
+const experiences = [dcCanada, innovapost];
+
+export default experiences;
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,19 +5,38 @@
       <p>
         Hello! I'm Ji Ho Oh (오지호) but I also go by Jay. I'm a 3rd-year undergrad at the University of Waterloo.<br /><br />
         I like creating user-facing software, UI/UX and all that, mostly because I want the things I make to look
-        <em>a e s t h e t i c</em>. This has led to me building websites, Android applications, and games. I also like learning
-        new libraries and frameworks, preferring to learn a little about a lot of things over specializing in one
-        aspect.<br /><br />Aside from coding, my hobbies include playing video games,
-        reading manga, and world-building.
+        <em>a e s t h e t i c</em>. This has led to me building websites, Android applications, and games. I also like
+        learning new libraries and frameworks, preferring to learn a little about a lot of things over specializing in
+        one aspect.<br /><br />Aside from coding, my hobbies include playing video games, reading manga, and
+        world-building.
       </p>
       <img src="../assets/img/about/profile-pic.png" alt="Profile Picture" />
+    </section>
+    <section id="experience">
+      <h2>Experience</h2>
+      <ul>
+        <li v-for="experience in experiences" :key="experience.id" @click="activeExperience = experience">
+          {{ experience.employer }}
+        </li>
+      </ul>
+      <div>
+        <h3>
+          <a :href="activeExperience.website">{{ activeExperience.employer }}</a> - {{ activeExperience.position }}
+        </h3>
+        <span>{{ activeExperience.dates }}</span>
+        <p v-html="activeExperience.description" />
+      </div>
     </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import experiences from "@/assets/content/about/experience.ts";
 
 @Component
-export default class About extends Vue {}
+export default class About extends Vue {
+  experiences = experiences;
+  activeExperience = experiences[0];
+}
 </script>
