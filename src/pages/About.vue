3dc0507001a243c80b608c5488b853968b077276
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-11-26 01:52:39 -0500

    Fixed transitions

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,37 +1,39 @@
 <template>
   <div id="about" class="page">
-    <h1>About Me</h1>
-    <section id="intro">
-      <p>
-        Hello! I'm Ji Ho Oh (오지호) but I also go by Jay. I'm a 3rd-year undergrad at the University of Waterloo.<br /><br />
-        I like creating user-facing software, UI/UX and all that, mostly because I want the things I make to look
-        <em>a e s t h e t i c</em>. This has led to me building websites, Android applications, and games. I also like
-        learning new libraries and frameworks, preferring to learn a little about a lot of things over specializing in
-        one aspect.<br /><br />Aside from coding, my hobbies include playing video games, reading manga, and
-        world-building.
-      </p>
-      <img src="../assets/img/about/profile-pic.png" alt="Profile Picture" />
-    </section>
-    <section id="experience">
-      <h2>Experience</h2>
-      <div id="experience-content">
-        <ul id="timeline">
-          <h4>
-            <li
-              v-for="experience in experiences"
-              :key="experience.id"
-              @click="activeExperience = experience"
-              :class="{ active: activeExperience === experience }"
-            >
-              {{ experience.employer }}
-            </li>
-          </h4>
-        </ul>
-        <transition name="shrink-grow-with-border" mode="out-in">
-          <ExperienceCard :key="activeExperience.id" :activeExperience="activeExperience" />
-        </transition>
-      </div>
-    </section>
+    <div id="about-content">
+      <h1>About Me</h1>
+      <section id="intro">
+        <p>
+          Hello! I'm Ji Ho Oh (오지호) but I also go by Jay. I'm a 3rd-year undergrad at the University of Waterloo.<br /><br />
+          I like creating user-facing software, UI/UX and all that, mostly because I want the things I make to look
+          <em>a e s t h e t i c</em>. This has led to me building websites, Android applications, and games. I also like
+          learning new libraries and frameworks, preferring to learn a little about a lot of things over specializing in
+          one aspect.<br /><br />Aside from coding, my hobbies include playing video games, reading manga, and
+          world-building.
+        </p>
+        <img src="../assets/img/about/profile-pic.png" alt="Profile Picture" />
+      </section>
+      <section id="experience">
+        <h2>Experience</h2>
+        <div id="experience-content">
+          <ul id="timeline">
+            <h4>
+              <li
+                v-for="experience in experiences"
+                :key="experience.id"
+                @click="activeExperience = experience"
+                :class="{ active: activeExperience === experience }"
+              >
+                {{ experience.employer }}
+              </li>
+            </h4>
+          </ul>
+          <transition name="shrink-grow-with-border" mode="out-in">
+            <ExperienceCard :key="activeExperience.id" :activeExperience="activeExperience" />
+          </transition>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
