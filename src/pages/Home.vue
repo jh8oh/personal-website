862ddfeb95fdcaf2cfeaf7d54e9833ccbe02ce7e
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-11-28 04:26:31 -0500

    Capitalized Developer

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,7 @@
     <div id="greeting">
       <span>Hi, I'm</span><br />
       <span id="name" :glitch-text="name">{{ name }}</span><br />
-      <span>Software developer, Programmer, Designer</span>
+      <span>Software Developer, Programmer, Designer</span>
       <br /><br />
       <Button :text="'Get to know me'" :buttonClass="'neon'" @clicked="goToAbout()" />
     </div>
