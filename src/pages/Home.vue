2f256a5d8cbd5e1d659624aa5de74a737054de51
Ji Ho Oh <ohjiho.ca@gmail.com>
2020-12-03 00:25:38 -0500

    Added primary and secondary color to button

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="home" class="page">
-    <div id="background-image" class="absolute-center"/>
+    <div id="background-image" class="absolute-center" />
     <div id="greeting">
       <span>Hi, I'm</span><br />
       <span id="name" glitch-text="Ji Ho Oh">Ji Ho Oh</span><br />
       <span>Software Developer, Programmer, Designer</span>
       <br /><br />
-      <Button :text="'Get to know me'" @clicked="goToAbout()" />
+      <Button :text="'Get to know me'" :buttonClass="'secondary'" @clicked="goToAbout()" />
     </div>
   </div>
 </template>
