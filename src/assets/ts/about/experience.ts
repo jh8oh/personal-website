e3392b0ef33df3bf6aeb21bd06c203b2de431232
Ji Ho Oh <ohjiho.ca@gmail.com>
2021-01-10 20:16:37 -0500

    Shorter dates

--- src/assets/ts/about/experience.ts
+++ src/assets/ts/about/experience.ts
@@ -28,7 +28,7 @@ const innovapost = new Experience(
   "Innovapost",
   "Android/Innovation Developer",
   "https://innovapost.com/",
-  "September - December 2020",
+  "Sept. - Dec. 2020",
   "Split between working on the Android-platform team and the 'Innovation Garage'.<br /><strong>Android Developer</strong><ul><li>Fixed production bugs and implemented new features for the Canada Post application</li><li>Worked in a agile-scrum environment</li></ul><strong>'Innovation Garage' Developer</strong><ul><li>Researched the possibility of implementing progressive registration and OAuth 2.0 on the app</li><li>Part of a pilot team to eliminate the need for ring scanners and instead use wrist-mounted PDTs which automatically (de)activate their scanner based on the gravimeter.</li></ul>"
 );
 
@@ -37,7 +37,7 @@ const dcCanada = new Experience(
   "DC Canada Education Publishing",
   "Web/Android Developer",
   "https://www.dc-canada.ca/",
-  "January - April 2020",
+  "Jan. - Apr. 2020",
   "Worked on maintaining the existing <a href=\"https://onestoryaday.ca\">One Story a Day website</a> and developing an app for it<br /><strong>Web Developer</strong><ul><li>Maintained and developed new contents and features on a WordPress powered website</li><li>Integrated MotionPay into the payment system for the Chinese domain which allowed customers to pay by scanning a QR code in Alipay/WeChat pay</ul><strong>Android Developer</strong><ul><li>Solo development and design of an Android eBook reader titled 'One Story a Day'</li><li>Connects with the LMS on the website to sync up users between the site</li></ul>"
 );
 
